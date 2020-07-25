84b66f4184bca723af65232405ecaf735c8c9e82
vieru <mixerandr3w001@gmail.com>
2020-07-25 20:00:54 +0300

    am separat clientul de master

--- src/client.nim
+++ src/client.nim
@@ -1,24 +1,11 @@
 import asyncdispatch
-from flippy import Image
-from flippy import save
-
-from lib/types import ImageData
 
 import lib/functions/getWindowName
 import lib/functions/screenshot
-import lib/functions/decodeImageBytes
-import lib/functions/ImageDataToImage
-import lib/functions/prepImage
 
 let base64Ss: string = screenshot()
 
-# writeFile("sal.tmp.txt", base64Ss)
-
-let data: ImageData = decode(base64Ss)
-
-let img = prepImage(ImageDataToImage(data))
-
-img.save("sal.tmp.png")
+writeFile("base64.tmp.txt", base64Ss)
 
 # proc main() {.async.} =
 #   echo getWindowName()
---
+++ src/master.nim
@@ -0,0 +1,16 @@
+from system import readFile
+
+from flippy import Image
+from flippy import save
+
+import lib/functions/decodeImageBytes
+import lib/functions/ImageDataToImage
+import lib/functions/prepImage
+
+let base64Ss = readFile("base64.tmp.txt")
+
+let data: ImageData = decode(base64Ss)
+
+let img = prepImage(ImageDataToImage(data))
+
+img.save("sal.tmp.png")
