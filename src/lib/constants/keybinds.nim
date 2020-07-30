b045d726c578bf763bad939723744a5e9cb6664d
d4rckh <m4dd0x.xxx@gmail.com>
2020-07-30 19:24:19 +0300

    am adaugat A-Z si 0-9 (le am facut separat)

--- src/lib/constants/keybinds.nim
+++ src/lib/constants/keybinds.nim
@@ -20,6 +20,42 @@ let keysReadable: keys = @[
     0xF8: "[EX SEL]", # VK_EXSEL
     0xE6: "[ICOCLR]", # VK_ICO_CLEAR
     0xE3: "[ICOHLP]", # VK_ICO_HELP
+    0x30: "0" # VK_KEY_0
+    0x31: "1" # VK_KEY_1
+    0x32: "2" # VK_KEY_2
+    0x33: "3" # VK_KEY_3
+    0x34: "4" # VK_KEY_4
+    0x35: "5" # VK_KEY_5
+    0x36: "6" # VK_KEY_6
+    0x37: "7" # VK_KEY_7
+    0x38: "8" # VK_KEY_8
+    0x39: "9" # VK_KEY_9
+    0x41: "A" # VK_KEY_A
+    0x42: "B" # VK_KEY_B
+    0x43: "C" # VK_KEY_C
+    0x44: "D" # VK_KEY_D
+    0x45: "E" # VK_KEY_E
+    0x46: "F" # VK_KEY_F
+    0x47: "G" # VK_KEY_G
+    0x48: "H" # VK_KEY_H
+    0x49: "I" # VK_KEY_I
+    0x4A: "J" # VK_KEY_J
+    0x4B: "K" # VK_KEY_K
+    0x4C: "L" # VK_KEY_L
+    0x4D: "M" # VK_KEY_M
+    0x4E: "N" # VK_KEY_N
+    0x4F: "O" # VK_KEY_O
+    0x50: "P" # VK_KEY_P
+    0x51: "Q" # VK_KEY_Q
+    0x52: "R" # VK_KEY_R
+    0x53: "S" # VK_KEY_S
+    0x54: "T" # VK_KEY_T
+    0x55: "U" # VK_KEY_U
+    0x56: "V" # VK_KEY_V
+    0x57: "W" # VK_KEY_W
+    0x58: "X" # VK_KEY_X
+    0x59: "Y" # VK_KEY_Y
+    0x5A: "Z" # VK_KEY_Z
     0x6A: "[NUMPAD *]", # VK_MULTIPLY
     0xFC: "[NONAME]", # VK_NONAME
     0x60: "[NUMPAD 0]", # VK_NUMPAD0
