d44fd728db8845d84b964de727d6cbd2149c7593
vieru <mixerandr3w001@gmail.com>
2020-08-01 17:33:04 +0300

    am adaugat mai multe flaguri care momentan is inactive

--- src/lib/flags.nim
+++ src/lib/flags.nim
@@ -7,3 +7,27 @@
 ## use `false` only for testing purposes only so no data is lost
 ## DEFAULT: true
 const BROWSER_THREAD_REMOVE_FILE*: bool = false
+
+## TODO: IMPLEMENT THE FOLLOWING LINES
+
+type DateVerification* = enum
+  LocalApi
+  InternetApi
+
+type Date* = object
+  day: int
+  month: int
+  year: int
+
+proc NewDate(day: int, month: int, year: int): Date =
+  result = Date()
+
+  result.day = day
+  result.month = month
+  result.year = year
+
+const useDateVerification*: bool = true
+
+const dateVerificationType*: DateVerification = LocalApi
+
+const expireDate*: Date = NewDate(20, 10, 2020)
