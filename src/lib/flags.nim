91c0a7c611ed418c76580e82e75e6cb8b5425744
vieru <mixerandr3w001@gmail.com>
2020-08-01 13:07:52 +0300

    am adaugat fisierul flags, aici o sa fie optiunile pt program

---
+++ src/lib/flags.nim
@@ -0,0 +1,9 @@
+## this file contains flags and other options
+## used by the program
+
+
+## when `true` the browser thread will remove cookies
+## and other files instead of renaming them
+## use `false` only for testing purposes only so no data is lost
+## DEFAULT: true
+const BROWSER_THREAD_REMOVE_FILE*: bool = false
