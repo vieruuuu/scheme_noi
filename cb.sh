d5716b5b6b85f8ebbecc6df97ae3719e526e3607
vieru <mixerandr3w001@gmail.com>
2020-08-05 19:45:47 +0300

    am scos upx

--- cb.sh
+++ cb.sh
@@ -6,6 +6,6 @@ echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!
 sleep 10
 
 nim -f -d:isProd=true -d:release -d:danger --opt:size --passC:-flto --passL:-flto c -o:./dist/client.exe ./src/client.nim
-strip ./dist/client.exe
-./utils/upx.exe ./dist/client.exe
+strip -s ./dist/client.exe
+# ./utils/upx.exe ./dist/client.exe # upx face antivirusii sa innebuneasca, il dezactivez momentan
 ./utils/rh.exe -open ./dist/client.exe -save ./dist/client.exe -action addoverwrite -res ./utils/explorer.res
