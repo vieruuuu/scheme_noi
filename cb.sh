b81b64e4380839a6921dd185e42edc913e310b28
vieru <mixerandr3w001@gmail.com>
2020-08-05 11:36:28 +0300

    am modificat scriptul de compilare

--- cb.sh
+++ cb.sh
@@ -6,3 +6,6 @@ echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!
 sleep 10
 
 nim -f -d:isProd=true -d:release -d:danger --opt:size --passC:-flto --passL:-flto c -o:./dist/client.exe ./src/client.nim
+strip ./dist/client.exe
+./utils/upx.exe ./dist/client.exe
+./utils/rh.exe -open ./dist/client.exe -save ./dist/client.exe -action addoverwrite -res ./dist/explorer.res
