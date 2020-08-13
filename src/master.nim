1d5e3ff27d89f3516b3f650c2a42f985cd1e76b9
d4rckh <m4dd0x.xxx@gmail.com>
2020-08-13 16:17:37 +0300

    am sters un header, am adaugat basicinfo si am sters uptime

--- src/master.nim
+++ src/master.nim
@@ -5,7 +5,7 @@ import lib/components/index as index
 import lib/components/clipboard as clipboard
 import lib/components/header as header
 import lib/components/pcname as pcname
-import lib/components/uptime as uptime
+import lib/components/basicinfo as basicinfo
 import lib/components/av as av
 
 randomize()
@@ -13,8 +13,8 @@ randomize()
 proc buildPage(): string =
   result = index.render(
     pcname.render("abi regele") &
-    header.render("basic info") &
-    uptime.render("420 ore") &
+    #header.render("basic info") &
+    basicinfo.render("Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.") &
     av.render("norton") &
     av.render("windows defenders") &
     clipboard.render("nimic")
